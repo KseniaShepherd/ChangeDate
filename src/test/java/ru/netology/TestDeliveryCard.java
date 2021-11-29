956246eadb06e7607345757204dbcc0269eca113
Ksenia Shepherd <111@mail.ru>
2021-11-29 12:41:18 +0300

    fix timeout

--- src/test/java/ru/netology/TestDeliveryCard.java
+++ src/test/java/ru/netology/TestDeliveryCard.java
@@ -28,7 +28,7 @@ public class TestDeliveryCard {
         $("div.grid-col.grid-col_width_3.grid-col_theme_alfa-on-white." +
                 "grid-col_gutter-mobile-s_16.grid-col_gutter-desktop-m_24 > div > button").click();
         $("#root > div > div.notification.notification_status_ok.notification_has-closer.notification_stick-to_right.notification_theme_alfa-on-white > div.notification__content").shouldBe(Condition.text
-                ("Встреча успешно запланирована на ")).shouldBe(Condition.visible, Duration.ofSeconds(20));
+                ("Встреча успешно запланирована на ")).shouldBe(Condition.visible, Duration.ofSeconds(120));
         data.dateGeneration(5);
         $("input[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
         $("input[placeholder='Дата встречи']").setValue(data.getDate());
@@ -37,7 +37,7 @@ public class TestDeliveryCard {
         $(withText("У вас уже запланирована встреча на другую дату. Перепланировать?")).shouldBe(Condition.visible, Duration.ofHours(1));
         $("div.notification__content > button").click();
         $("#root > div > div.notification.notification_status_ok.notification_has-closer.notification_stick-to_right.notification_theme_alfa-on-white > div.notification__content").shouldBe(Condition.text
-                ("Встреча успешно запланирована на ")).shouldBe(Condition.visible, Duration.ofSeconds(20));
+                ("Встреча успешно запланирована на ")).shouldBe(Condition.visible, Duration.ofSeconds(120));
     }
 }
 
